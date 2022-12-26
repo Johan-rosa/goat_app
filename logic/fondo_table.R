f45fb83d4bacefb1f5045ea6337ddebe0567a144
Johan Rosa <johan.rosaperez@gmail.com>
2022-12-26 10:33:52 -0400

    New table with nested details

--- logic/fondo_table.R
+++ logic/fondo_table.R
@@ -7,11 +7,12 @@ library(tidyr)
 library(stringr)
 library(readxl)
 library(reactable)
+library(purrr)
 
 
 # Preparing data ----------------------------------------------------------
 
-aportes <- read_excel("../fondo/aportes.xlsx", sheet = "Ingreso") |>
+aportes <- read_excel("./data/aportes.xlsx", sheet = "Ingreso") |>
   janitor::clean_names() |>
   mutate(
     fecha = as.Date(fecha),
@@ -21,7 +22,7 @@ aportes <- read_excel("../fondo/aportes.xlsx", sheet = "Ingreso") |>
     month = forcats::fct_reorder(label_fecha, fecha)
     )
 
-gastos <- read_excel("../fondo/aportes.xlsx", sheet = "Gastos") |>
+gastos <- read_excel("./data/aportes.xlsx", sheet = "Gastos") |>
   janitor::clean_names() |>
   mutate(
     fecha = as.Date(fecha),
@@ -44,10 +45,43 @@ aportes_table <- aportes |>
   select(all_of(dates_order))
 
 
+aportes_table_2 <- aportes %>%
+  group_by(miembro) %>%
+  nest() %>%
+  mutate(
+    cantidad_aportes = map_dbl(data, nrow),
+    total = map_dbl(data, ~sum(.x[["aporte"]], na.rm = TRUE)),
+    data = map(data, ~.x[,c("fecha", "aporte")] %>% arrange(desc(fecha)))
+    ) %>%
+  select(miembro, cantidad_aportes, total, data) %>% 
+  arrange(desc(cantidad_aportes))
 
 # html table --------------------------------------------------------------
 
-aportes_table |>
+tabla_aportes <- aportes_table_2 %>%
+  select(-data) %>%
+  arrange(desc(total)) %>% 
+  reactable(
+    fullWidth = FALSE,
+    searchable = TRUE,
+    pagination = FALSE,
+    defaultColDef = colDef(headerClass = "header"),
+    columns = list(
+      miembro = colDef(name = "Miembro"),
+      cantidad_aportes = colDef(name = "Aportes"),
+      total = colDef("Total", format = colFormat(separators = TRUE))
+    ),
+    details = function(index) {
+      htmltools::div(
+        style = "padding: 1rem",
+        reactable(aportes_table_2[["data"]][[index]], outlined = TRUE)
+      )
+    },
+    class = "aportes-table"
+  )
+
+# Table 1
+tabla_aportes1 <- aportes_table |>
   setNames(str_to_sentence(names(aportes_table))) |> 
   reactable(
     defaultColDef = colDef(width = 110),
